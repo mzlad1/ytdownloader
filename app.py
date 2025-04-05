9bcef683d7b53ae00785643fe76b17dab28c3e13
Mohammad ladadweh <mohalada1@gmail.com>
2025-04-05 03:52:38 +0200

    last1

--- app.py
+++ app.py
@@ -247,5 +247,5 @@ def create_static_files():
 </html>''')
 
 if __name__ == '__main__':
-    create_static_files()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
